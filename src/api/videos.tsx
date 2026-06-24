97bf948c946f372a24daf47e9a149f79100317ab
EKIKOUKA <45888850+EKIKOUKA@users.noreply.github.com>
2026-06-24 16:03:35 +0900

    1

--- src/api/videos.tsx
+++ src/api/videos.tsx
@@ -1,5 +1,5 @@
 export async function getVideos() {
-    const VITE_API_BASE_URL= import.meta.env('VITE_API_BASE_URL');
+    const VITE_API_BASE_URL= import.meta.env.VITE_API_BASE_URL;
 
     const response = await fetch(
         `${VITE_API_BASE_URL}"/shadowing/fetch_videos`
